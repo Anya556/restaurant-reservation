8dc69f015c7b9346961c067d99ab3c5337fbbd47
Anna <gstudentseo@gmail.com>
2023-08-24 21:54:20 +0200

    Reservations updating & Viewing State

--- src/features/reservationSlice.ts
+++ src/features/reservationSlice.ts
@@ -1,12 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-const initialState = {
+export interface ResertionState {
+  value: string[];
+}
+
+const initialState: ResertionState = {
   value: [],
 };
 
 export const reservationsSlice = createSlice({
   name: 'reservations',
   initialState,
-  reducers: {},
+  reducers: {
+    addReservation: (state, action: PayloadAction<string>) => {
+      state.value.push(action.payload);
+    },
+    removeReservation: (state, action: PayloadAction<number>) => {
+      state.value.splice(action.payload, 1);
+    },
+  },
 });
+
+export const { addReservation, removeReservation } = reservationsSlice.actions;
 export default reservationsSlice.reducer;
